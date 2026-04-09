1e49869a9549b274569cd5a4391d6806c5fb8f81
60725 <60725@etu.he2b.be>
2026-04-10 00:16:48 +0200

    fix: ajout H2 + correction démarrage app

---
+++ target/classes/data.sql
@@ -0,0 +1,92 @@
+INSERT INTO groupe (lettre) VALUES ('A');
+INSERT INTO groupe (lettre) VALUES ('B');
+INSERT INTO groupe (lettre) VALUES ('C');
+INSERT INTO groupe (lettre) VALUES ('D');
+INSERT INTO groupe (lettre) VALUES ('E');
+INSERT INTO groupe (lettre) VALUES ('F');
+INSERT INTO groupe (lettre) VALUES ('G');
+INSERT INTO groupe (lettre) VALUES ('H');
+INSERT INTO groupe (lettre) VALUES ('I');
+INSERT INTO groupe (lettre) VALUES ('J');
+INSERT INTO groupe (lettre) VALUES ('K');
+INSERT INTO groupe (lettre) VALUES ('L');
+
+-- Groupe A
+INSERT INTO equipe (nom, groupe_id) VALUES ('Mexique', 1);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Afrique du Sud', 1);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Corée du Sud', 1);
+INSERT INTO equipe (nom, groupe_id) VALUES ('République Tchèque', 1);
+
+-- Groupe B
+INSERT INTO equipe (nom, groupe_id) VALUES ('Canada', 2);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Bosnie-Herzégovine', 2);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Qatar', 2);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Suisse', 2);
+
+-- Groupe C
+INSERT INTO equipe (nom, groupe_id) VALUES ('Brésil', 3);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Maroc', 3);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Haïti', 3);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Ecosse', 3);
+
+-- Groupe D
+INSERT INTO equipe (nom, groupe_id) VALUES ('Etats-Unis', 4);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Paraguay', 4);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Australie', 4);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Turquie', 4);
+
+-- Groupe E
+INSERT INTO equipe (nom, groupe_id) VALUES ('Allemagne', 5);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Curaçao', 5);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Côte d''Ivoire', 5);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Equateur', 5);
+
+-- Groupe F
+INSERT INTO equipe (nom, groupe_id) VALUES ('Pays-Bas', 6);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Japon', 6);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Suède', 6);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Tunisie', 6);
+
+-- Groupe G
+INSERT INTO equipe (nom, groupe_id) VALUES ('Belgique', 7);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Egypte', 7);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Iran', 7);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Nouvelle-Zélande', 7);
+
+-- Groupe H
+INSERT INTO equipe (nom, groupe_id) VALUES ('Espagne', 8);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Cap-Vert', 8);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Arabie Saoudite', 8);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Uruguay', 8);
+
+-- Groupe I
+INSERT INTO equipe (nom, groupe_id) VALUES ('France', 9);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Sénégal', 9);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Irak', 9);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Norvège', 9);
+
+-- Groupe J
+INSERT INTO equipe (nom, groupe_id) VALUES ('Argentine', 10);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Algérie', 10);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Autriche', 10);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Jordanie', 10);
+
+-- Groupe K
+INSERT INTO equipe (nom, groupe_id) VALUES ('Portugal', 11);
+INSERT INTO equipe (nom, groupe_id) VALUES ('RD Congo', 11);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Ouzbékistan', 11);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Colombie', 11);
+
+-- Groupe L
+INSERT INTO equipe (nom, groupe_id) VALUES ('Angleterre', 12);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Croatie', 12);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Ghana', 12);
+INSERT INTO equipe (nom, groupe_id) VALUES ('Panama', 12);
+
+-- NOM 
+
+INSERT INTO users (username, password) VALUES ('Axel', '');
+INSERT INTO users (username, password) VALUES ('Imrane', '');
+INSERT INTO users (username, password) VALUES ('Noah', '');
+INSERT INTO users (username, password) VALUES ('Ayman', '');
+INSERT INTO users (username, password) VALUES ('Abed', '');
